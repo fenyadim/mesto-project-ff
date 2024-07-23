34f2202fb3bcff8f6862479e5a8f8f20b35e6e2b
fenyadim <dimos9813@gmail.com>
2024-07-23 13:17:30 +0800

    6 спринт

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,40 +1,51 @@
 import { initialCards } from "./cards";
-import { createCard, deleteCard, sendLike } from '../components/card';
-import { closePopup, openImagePopup, openPopup } from '../components/modal';
-import '../pages/index.css';
+import { createCard, deleteCard, sendLike } from "../components/card";
+import { closePopup, openPopup } from "../components/modal";
+import "../pages/index.css";
 
-const placeListElement = document.querySelector('.places__list');
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-const profileName = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__description');
+const placeListElement = document.querySelector(".places__list");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileAddButton = document.querySelector(".profile__add-button");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-const popupEditWrapper = document.querySelector('.popup_type_edit');
-const popupNewCardWrapper = document.querySelector('.popup_type_new-card');
+const popupEditWrapper = document.querySelector(".popup_type_edit");
+const popupNewCardWrapper = document.querySelector(".popup_type_new-card");
+const popupImageWrapper = document.querySelector(".popup_type_image");
+const popupImage = popupImageWrapper.querySelector(".popup__image");
+const popupCaption = popupImageWrapper.querySelector(".popup__caption");
 
-const editFormElement = document.forms['edit-profile'];
+const editFormElement = document.forms["edit-profile"];
 const nameInput = editFormElement.elements.name;
 const jobInput = editFormElement.elements.description;
 
-const createFormElement = document.forms['new-place'];
-const cardNameInput = createFormElement.elements['place-name'];
+const createFormElement = document.forms["new-place"];
+const cardNameInput = createFormElement.elements["place-name"];
 const cardUrlInput = createFormElement.elements.link;
 
 nameInput.value = profileName.textContent;
 jobInput.value = profileDescription.textContent;
 
-initialCards.forEach(({
-                          name,
-                          link
-                      }) => placeListElement.append(createCard(name, link, deleteCard, sendLike, openImagePopup)));
+export function openImagePopup(name, link) {
+    openPopup(popupImageWrapper);
+    popupImage.src = link;
+    popupImage.alt = name;
+    popupCaption.textContent = name;
+}
 
-profileEditButton.addEventListener('click', () => {
-    openPopup(popupEditWrapper)
-})
+initialCards.forEach(({ name, link }) =>
+    placeListElement.append(
+        createCard(name, link, deleteCard, sendLike, openImagePopup)
+    )
+);
 
-profileAddButton.addEventListener('click', () => {
-    openPopup(popupNewCardWrapper)
-})
+profileEditButton.addEventListener("click", () => {
+    openPopup(popupEditWrapper);
+});
+
+profileAddButton.addEventListener("click", () => {
+    openPopup(popupNewCardWrapper);
+});
 
 function editFormSubmit(e) {
     e.preventDefault();
@@ -45,15 +56,17 @@ function editFormSubmit(e) {
     closePopup(popupEditWrapper);
 }
 
-editFormElement.addEventListener('submit', editFormSubmit)
+editFormElement.addEventListener("submit", editFormSubmit);
 
 function createNewCardSubmit(e) {
     e.preventDefault();
     const cardName = cardNameInput.value;
     const link = cardUrlInput.value;
-    placeListElement.prepend(createCard(cardName, link, deleteCard, sendLike, openImagePopup));
+    placeListElement.prepend(
+        createCard(cardName, link, deleteCard, sendLike, openImagePopup)
+    );
     createFormElement.reset();
     closePopup(popupNewCardWrapper);
 }
 
-createFormElement.addEventListener('submit', createNewCardSubmit);
+createFormElement.addEventListener("submit", createNewCardSubmit);
